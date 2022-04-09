3b9c7756c5d4b0a3564147ed975b4b986827ac77
FernandoRL <rlfernando7@gmail.com>
2022-04-08 23:03:26 -0500

    Assign dashboard ass default path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,8 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 const routes: Routes = [
   // path: '/dashboard' PagesRoutingModule
   // path: 'login' or 'register' AuthRoutingModule
+  
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: '**', component: NopagefoundComponent }
 ];
 
