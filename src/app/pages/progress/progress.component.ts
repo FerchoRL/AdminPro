457eab76f8d7fe9938ffc8836b67f042825131ba
FernandoRL <rlfernando7@gmail.com>
2022-04-09 20:03:32 -0500

    Implement striped progress bar on progress page

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -2,7 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-progress',
-    templateUrl: './progress.component.html'
+    templateUrl: './progress.component.html',
+    styleUrls: [
+        './progress.component.css'
+    ]
 })
 
-export class ProgressComponent{}
+export class ProgressComponent {
+    futbolProgress: number = 25;
+    testingProgress: number = 55;
+
+    getProgressPercentage1() {
+        return `${this.futbolProgress}%`
+    }
+
+    getProgressPercentage2() {
+        return `${this.testingProgress}%`
+    }
+}
