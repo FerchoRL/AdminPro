f08b00cc52fd40d5a50cd9aef1f46e6fb01de395
FernandoRL <rlfernando7@gmail.com>
2022-04-11 18:20:24 -0500

    Implement check theme with OnInit and query selector

--- src/app/pages/accountsettings/account-setting.component.ts
+++ src/app/pages/accountsettings/account-setting.component.ts
@@ -9,9 +9,12 @@ export class AccountSettingsComponent implements OnInit{
 
     //Get element from index with theme as ID
     public linkTheme = document.querySelector('#theme');
+    //Get elements with selector class
+    public links!: NodeListOf<Element>;
 
     ngOnInit(): void{
-
+        this.links = document.querySelectorAll('.selector');
+        this.checkCurrentTheme();
     }
 
     changeTheme( theme: string ){
@@ -20,5 +23,24 @@ export class AccountSettingsComponent implements OnInit{
         this.linkTheme?.setAttribute('href', url);
         //save in local storage
         localStorage.setItem('theme', url);
+        this.checkCurrentTheme();
+    }
+
+    checkCurrentTheme(){
+        
+        this.links.forEach( element =>{
+            //to deselect all the items
+            element.classList.remove('working');
+            //get theme name
+            const btnTheme = element.getAttribute('data-theme');
+            //build url
+            const themeUrl = `./assets/css/colors/${ btnTheme }.css`;
+            //get current theme
+            const currentTheme = this.linkTheme?.getAttribute('href');
+            // If both are same then add working class
+            if(themeUrl === currentTheme){
+                element.classList.add('working');
+            }
+        })
     }
 }
