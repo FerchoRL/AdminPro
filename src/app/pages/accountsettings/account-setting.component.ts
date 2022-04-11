16dcc73084a3545b96f9f1ea864616583652778a
FernandoRL <rlfernando7@gmail.com>
2022-04-11 17:57:03 -0500

    Initialize theme when start the application and save in local storage

--- src/app/pages/accountsettings/account-setting.component.ts
+++ src/app/pages/accountsettings/account-setting.component.ts
@@ -1,8 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-account-settings',
     templateUrl: './account-settings.component.html'
 })
 
-export class AccountSettingsComponent{}
+export class AccountSettingsComponent implements OnInit{
+
+    //Get element from index with theme as ID
+    public linkTheme = document.querySelector('#theme');
+
+    ngOnInit(): void{
+
+    }
+
+    changeTheme( theme: string ){
+        //Create URL with new theme and apply to theme id
+        const url = `./assets/css/colors/${ theme }.css`
+        this.linkTheme?.setAttribute('href', url);
+        //save in local storage
+        localStorage.setItem('theme', url);
+    }
+}
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,8 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-pages',
     templateUrl: './pages.component.html'
 })
 
-export class PagesComponent{}
+export class PagesComponent implements OnInit {
+
+    //Get element from index with theme as ID
+    public linkTheme = document.querySelector('#theme');
+    ngOnInit(): void {
+
+        //setup a theme according local storage
+
+        //Verify local storage theme and build the URL
+        let url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
+        //Add URL to href theme
+        this.linkTheme?.setAttribute('href',url);
+        //Set url theme to localstorage
+        localStorage.setItem('theme',url);
+    }
+}
