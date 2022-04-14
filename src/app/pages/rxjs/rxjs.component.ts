5d6a41f123565e772a923cd47b9fcca2f4857971
FernandoRL <rlfernando7@gmail.com>
2022-04-13 23:07:21 -0500

    Implement retry in observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, retry } from "rxjs";
 
 @Component({
     selector: 'app-rxjs',
@@ -10,8 +10,8 @@ export class RxjsComponent {
 
     constructor() {
         //Signo de $ al hacer referencia a un nuevo observable
+        let i = -1;
         const obs$ = new Observable(observer => {//Se genera una subscripcion
-            let i = -1;
             const interval = setInterval( () => {
                 i++;
                 observer.next(i);//Se ejecuta infinitamente
@@ -27,7 +27,10 @@ export class RxjsComponent {
         });
         
         //Hay que suscribirse para que se ejecute el codigo de la susbcripcion
-        obs$.subscribe({//Dentro del subscribe hay 3 opciones
+        //El pipe transforma y puede rehacer la informacion
+        obs$.pipe(
+            retry()//Retry intenta nuevamente el observable
+        ).subscribe({//Dentro del subscribe hay 3 opciones
             next: value => console.log('Subs: ', value),//Opcion next que es cuando se ejecuta
             error: err => console.warn('Error: ', err),//Opcion error 
             complete: () => console.info('Obs terminado')//Opcion complete
