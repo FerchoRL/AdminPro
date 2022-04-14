d9450e52b239d4ae46ab856c7589fee356482c2a
FernandoRL <rlfernando7@gmail.com>
2022-04-13 23:36:24 -0500

    implement the return observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -9,31 +9,37 @@ import { Observable, retry } from "rxjs";
 export class RxjsComponent {
 
     constructor() {
-        //Signo de $ al hacer referencia a un nuevo observable
-        let i = -1;
-        const obs$ = new Observable(observer => {//Se genera una subscripcion
-            const interval = setInterval( () => {
-                i++;
-                observer.next(i);//Se ejecuta infinitamente
-                if(i == 4){//Cancelar intervalo cuando cumpla la condicion
-                    clearInterval(interval);
-                    observer.complete();
-                }
-                if(i == 2){
-                    observer.error('Generando un error')
-                }
-                //console.log('jelouda');
-            },1000)
-        });
-        
+
         //Hay que suscribirse para que se ejecute el codigo de la susbcripcion
         //El pipe transforma y puede rehacer la informacion
-        obs$.pipe(
+        this.returnObservable().pipe(
             retry()//Retry intenta nuevamente el observable
         ).subscribe({//Dentro del subscribe hay 3 opciones
             next: value => console.log('Subs: ', value),//Opcion next que es cuando se ejecuta
             error: err => console.warn('Error: ', err),//Opcion error 
             complete: () => console.info('Obs terminado')//Opcion complete
         });
+    }
+
+    returnObservable(): Observable<number> {//Le indico el tipo de observable
+        //Signo de $ al hacer referencia a un nuevo observable
+        let i = -1;
+        const obs$ = new Observable<number>(observer => {//Se genera una subscripcion
+            const interval = setInterval(() => {
+                i++;
+                observer.next(i);//Se ejecuta infinitamente
+                if (i == 4) {//Cancelar intervalo cuando cumpla la condicion
+                    clearInterval(interval);
+                    observer.complete();
+                }
+                if (i == 2) {
+                    observer.error('Generando un error')
+                }
+                //console.log('jelouda');
+            }, 1000)
+        });
+
+        return obs$;
+
     }
 }
