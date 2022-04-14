07f8e3300158744a9dc1480729a0d548e14df4bb
FernandoRL <rlfernando7@gmail.com>
2022-04-13 21:17:07 -0500

    implement promise to call get API

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -5,23 +5,40 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: 'promises.component.html'
 })
 
-export class PromisesComponent implements OnInit{
-    constructor() {}
+export class PromisesComponent implements OnInit {
+    constructor() { }
+    //PROMISE EXAMPLE
+    // ngOnInit(): void {
+    //     const mypromise = new Promise( (resolve, reject) => {
+    //         if (false){
+    //             resolve('Hola Mundo')//Si sale bien
+    //         }else{
+    //             reject('Algo Salio mal')//Si la promesa no se resuelve
+    //         }
+    //     });
+    //     //Cuando la promesa se hace correctamente
+    //     mypromise.then( (msg) => {
+    //         console.log('Termine')
+    //         console.log(msg)
+    //     }).catch(error => console.log('Error en mi promesa',error));//Si sale mal cacho el error
+
+    //     console.log('Fin del Init')
+    // }
 
     ngOnInit(): void {
-        const mypromise = new Promise( (resolve, reject) => {
-            if (false){
-                resolve('Hola Mundo')//Si sale bien
-            }else{
-                reject('Algo Salio mal')//Si la promesa no se resuelve
-            }
-        });
-        //Cuando la promesa se hace correctamente
-        mypromise.then( (msg) => {
-            console.log('Termine')
-            console.log(msg)
-        }).catch(error => console.log('Error en mi promesa',error));//Si sale mal cacho el error
 
-        console.log('Fin del Init')
+        this.getUser().then(users => {//Trabaja con la promesa resuelta
+            console.log(users)
+        });
+    }
+
+    getUser() {
+        return new Promise(resolve => {//Promise that call get API and receive a response
+            fetch('https://reqres.in/api/users')
+                //Get json
+                .then(resp => resp.json())
+                // get body.data and resolve the promise
+                .then(body => resolve(body.data));
+        });
     }
 }
