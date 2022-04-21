3776eff42115185b0904d63f0ad376a86306810f
FernandoRL <rlfernando7@gmail.com>
2022-04-20 19:30:44 -0500

    Implement updates on Breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,28 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
 import { ActivationEnd, Router } from "@angular/router";
-import { filter, map } from "rxjs";
+import { filter, map, Subscription } from "rxjs";
 
 @Component({
     selector: 'app-breadcrumbs',
     templateUrl: './breadcrumbs.component.html'
 })
-export class BreadcrumbsComponent{
+export class BreadcrumbsComponent implements OnDestroy {
     public titlePage: string = '';
-    constructor( private router: Router){
-        this.getDataFromRouter();
+    public titleSubs$: Subscription | undefined;
+    constructor(private router: Router) {
+        this.titleSubs$ = this.getDataFromRouter()
+            //Desestructurar informacion
+            .subscribe(({ titlePage }) => {
+                this.titlePage = titlePage;
+                document.title = `AdminPro - ${titlePage}`;
+            });
+    }
+    //Implement unsubscribe when user logout
+    ngOnDestroy(): void {
+        this.titleSubs$?.unsubscribe();
     }
 
-    getDataFromRouter(){
-        this.router.events
-        .pipe(
-            filter( (event: any) => event instanceof ActivationEnd),
-            filter( (event: ActivationEnd) => event.snapshot.firstChild === null),
-            map( (event: ActivationEnd) => event.snapshot.data)
-        )
-        //Desestructurar informacion
-        .subscribe(({ titlePage }) =>{
-            this.titlePage = titlePage;
-            document.title = `AdminPro - ${titlePage}`;
-        });
+    getDataFromRouter() {
+        //al llamar al router se cuenta con los siguientes eventos
+        //La data que necesitamos esta dentro de ActivationEnd -> snapshot -> data. Como en el proyecto hay dos usos de router link, nos aparecen dos activation end. Por eso los filtramos por el que tenga firstChild igual a null (seria el router link padre)
+        return this.router.events
+            .pipe(
+                filter((event: any) => event instanceof ActivationEnd),
+                filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+                map((event: ActivationEnd) => event.snapshot.data)
+            )
     }
 }
