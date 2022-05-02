7b4f97496138ff0742f02430615ea09a0554e8cd
FernandoRL <rlfernando7@gmail.com>
2022-05-02 01:37:04 -0500

    Implement reactive forms to capture register information

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-register',
@@ -6,4 +7,18 @@ import { Component } from "@angular/core";
     styleUrls: ['./register.component.css']
 })
 
-export class RegisterComponent{}
+export class RegisterComponent{
+    public registerForm = this.fb.group({
+        userName: ['Ferchower', [Validators.required, Validators.minLength(3)]],
+        email: ['ferchower@gmail.com', [Validators.required, Validators.email]],
+        password: ['12345', [Validators.required]],
+        password2: ['12345', [Validators.required]],
+        terms: [true, Validators.required]
+    });
+
+    constructor( private fb: FormBuilder){}
+
+    createUser(){
+        console.log(this.registerForm.value);
+    }
+}
