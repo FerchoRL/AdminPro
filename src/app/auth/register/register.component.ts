bf283247fd92b89613ca0f53099b97fb545afc5c
FernandoRL <rlfernando7@gmail.com>
2022-05-02 01:57:14 -0500

    implement validations on register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,17 +8,31 @@ import { FormBuilder, Validators } from "@angular/forms";
 })
 
 export class RegisterComponent{
+
+    public formSubmitted = false;
     public registerForm = this.fb.group({
-        userName: ['Ferchower', [Validators.required, Validators.minLength(3)]],
+        userName: ['ferchi', [Validators.required, Validators.minLength(3)]],
         email: ['ferchower@gmail.com', [Validators.required, Validators.email]],
         password: ['12345', [Validators.required]],
         password2: ['12345', [Validators.required]],
-        terms: [true, Validators.required]
+        terms: [true, Validators.requiredTrue]
     });
 
     constructor( private fb: FormBuilder){}
 
     createUser(){
+        this.formSubmitted = true;
         console.log(this.registerForm.value);
+        if (this.registerForm.valid) {
+            console.log('Posteando formulario');
+        }else{
+            console.log('Error en el form')
+        }
+    }
+    invalidField( field: string): boolean {
+        if(this.registerForm.get(field)?.invalid && this.formSubmitted){
+            return true;
+        }
+        return false;
     }
 }
