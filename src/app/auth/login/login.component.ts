aa7177d45b60885e91c421d2b321e38171e93fc1
FernandoRL <rlfernando7@gmail.com>
2022-05-02 17:47:05 -0500

    create service and interface for login method and call login AP

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,43 @@
 import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { UserService } from "src/app/services/user.service";
+import Swal from "sweetalert2";
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LogginComponent{
+export class LogginComponent {
 
-    constructor(private router: Router){}
+    constructor(private router: Router,
+        private fb: FormBuilder,
+        private userService: UserService) { }
 
-    login(){
-        this.router.navigateByUrl('/');
+    //TODO: Implement validations for email and pass
+
+
+    //TODO: FormBuilder group is deprecated
+    public loginForm = this.fb.group({
+        email: ['joseputop@gmail.com', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]],
+        remember: [false]
+    });
+
+    login() {
+        //TODO: subscribe is deprecated I have to check
+        // this.router.navigateByUrl('/');
+        this.userService.login(this.loginForm.value)
+            .subscribe(resp => {
+                console.log(resp)
+            }, (err) => {
+                //Estas con las rutas en donde podria encontrar mis errores provenientes del backend
+                //TODO: implement validations when didn't send email or password
+                let errorMsg = err.error.msg;
+                console.log(errorMsg);
+                Swal.fire('Error', errorMsg, 'error');
+            });
 
     }
 
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
+import { LoginForm } from "../interfaces/login-form.interfaces";
 import { RegisterForm } from "../interfaces/register-form.interfaces";
 
 const base_url = environment.base_url;
@@ -12,9 +13,16 @@ export class UserService {
     constructor( private http: HttpClient){}
 
     //Hago mi interfaz RegisterForm para poder acceder a sus propiedades y el tipado
+    //Register
     createUser( formData: RegisterForm){
         //Call create user api from my backend
         //Return an observable so I have to subscribe
         return this.http.post(`${ base_url}/users`,formData);
+    }
+    //Login
+    login( formData: LoginForm){
+        //Call create user api from my backend
+        //Return an observable so I have to subscribe
+        return this.http.post(`${ base_url}/login`,formData);
     }
 }
