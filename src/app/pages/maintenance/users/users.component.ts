ca46166f2afc552a0a43046966f24d399b77891f
Fernando RL <rlfernando7@gmail.com>
2022-08-13 00:34:10 -0500

    Implement correct pagination on user page

--- src/app/pages/maintenance/users/users.component.ts
+++ src/app/pages/maintenance/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Validators } from "@angular/forms";
 import { User } from "src/app/models/user.model";
 import { UserService } from "src/app/services/user.service";
 
@@ -10,17 +11,32 @@ import { UserService } from "src/app/services/user.service";
     ]
 })
 
-export class UsersComponent implements OnInit{
+export class UsersComponent implements OnInit {
 
-    public totalUsers: number =0;
+    public totalUsers: number = 0;
     public users: User[] = [];
-    constructor( private userService: UserService){}
+    public fromPage: number = 0;
+
+    constructor(private userService: UserService) { }
 
     ngOnInit(): void {
-        this.userService.loadUsers()
-        .subscribe( ({countUsers, allUsers}) => {
-            this.totalUsers = countUsers;
-            this.users = allUsers;
-        })
+        this.refreshUsers();
+    }
+
+    //Funcion que me ayuda a actualizar la pagina cuando cambie de paginacion
+    refreshUsers() {
+        //Envio 5 como limite para mostrar de 5 en 5
+        this.userService.loadUsers(this.fromPage, 5)
+            .subscribe(({ countUsers, allUsers }) => {
+                this.totalUsers = countUsers;
+                this.users = allUsers;
+            });
+    }
+
+    //Implement pagination function
+    changePage(value: number) {
+        this.fromPage += value;
+        this.fromPage = this.fromPage < 0 ? 0 : this.fromPage >= this.totalUsers ? this.fromPage -= value : this.fromPage;
+        this.refreshUsers();
     }
 }
